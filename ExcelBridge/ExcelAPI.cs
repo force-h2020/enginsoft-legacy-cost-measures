6e9d65107c53169ada9be60b36520feac11ff3da
Daniele Callegaro <daniele.callegaro.90@gmail.com>
2020-02-28 15:23:49 +0100

    Initial Commit

---
+++ ExcelBridge/ExcelAPI.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelBridge
+{
+    class ExcelAPI
+    {
+        private String filename;
+        private Application excel;
+        private Workbook workBook;
+        private Worksheet sheet;
+
+        /**
+         * Costruisce l'oggetto Excel API
+         * Apre il file passato come parametro, seleziona il primo foglio
+         * e disabilita di default il calcolo automatico
+         **/
+        public ExcelAPI(string filename)
+        {
+            this.filename = filename;
+            this.excel = new Application();
+            this.workBook = excel.Workbooks.Open(filename);
+            this.changeSheet(1);
+            this.disableAutomaticCalculation();
+        }
+
+        /**
+         * Cambia il foglio di default
+         */
+        public void changeSheet(int sheetNumber)
+        {
+            this.sheet = (Worksheet)this.workBook.Sheets[1];
+        }
+
+        /**
+         * Scrive un valore su una cella
+         */
+        public void writeToCell(int i, int j, Object value)
+        {
+            sheet.Cells[i, j].Value = value;
+        }
+
+        /**
+         *  Legge il valore da una cella
+         */
+        public Object getCellValue(int i, int j)
+        {
+            return sheet.Cells[i, j].Value;
+        }
+
+
+        /**
+         * Esegue una Macro 
+         */
+        public void runMacro(String macroname)
+        {
+            excel.Run(macroname);
+        }
+
+        /**
+         * Salva le modifiche nel file corrente 
+         */
+        public void save()
+        {
+            workBook.Save();
+        }
+
+        /**
+         * Chiude il documento corrente 
+         * 
+         */
+        public void close()
+        {
+            this.workBook.Close();
+        }
+
+        /**
+         * Disabilita il calcolo automatico
+         * 
+         */
+        public void disableAutomaticCalculation()
+        {
+            this.excel.Calculation = XlCalculation.xlCalculationManual;
+        }
+
+        /**
+         * Abilita il calcolo automatico
+         * 
+         */
+        public void enableAutomaticCalculation()
+        {
+            this.excel.Calculation = XlCalculation.xlCalculationAutomatic;
+        }
+
+
+        /**
+         * Esegue il calcolo 
+         */
+        public void calculate()
+        {
+            this.excel.Calculate();
+        }
+    }
+}
---
+++ ExcelBridge/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using CommandLine;
+
+
+namespace ExcelBridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            var isValid = CommandLine.Parser.Default.ParseArguments(args, options);
+            if(isValid)
+            {
+                ExcelAPI api = new ExcelAPI(options.InputFile);
+                switch (options.command)
+                {
+                    //Scrive il valore su una cella
+                    case "writetocell":
+                        api.writeToCell(options.row, options.col, options.value);
+                        break;
+                    //Esegue una macro
+                    case "runmacro":
+                        api.runMacro(options.macro);
+                        break;
+                    //Legge una cella e stampa a video il risultato
+                    case "readcell":
+                        Console.Write(api.getCellValue(options.row, options.col).ToString());
+                        break;
+                }
+                if (options.calculate)
+                    api.calculate();
+                api.save();
+                api.close();
+            }
+        }        
+    }
+
+    //Classe che rappresenta i parametri della CLI(Command Line Interface)
+    class Options
+    {
+        //Il file di input
+        [Option("file", Required = true, HelpText = "Input .xlsx file")]
+        public string InputFile { get; set; }
+
+        //Il comando da eseguire
+        [Option("command", Required = true, HelpText = "Command")]
+        public string command { get; set; }
+
+        //Il foglio su cui eseguire il comando, default 1
+        [Option("sheet", Required = false, HelpText = "The active sheet", DefaultValue = 1)]
+        public int sheet { get; set; }
+
+        //Abilita il calcolo alla fine del comando
+        [Option("calculate", Required = false, HelpText = "Enable calculation at the end of command", DefaultValue = false)]
+        public Boolean calculate { get; set; }
+
+        //La riga
+        [Option("row", Required = false, HelpText = "The row")]
+        public int row { get; set; }
+
+        //La Colonna
+        [Option("col", Required = false, HelpText = "The col")]
+        public int col { get; set; }
+
+        //Il valore
+        [Option("value", Required = false, HelpText = "Value")]
+        public string value { get; set; }
+
+        //Il nome della macro
+        [Option("macro", Required = false, HelpText = "Macro name")]
+        public string macro { get; set; }
+    }
+}
